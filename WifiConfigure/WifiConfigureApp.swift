690fda4145ab9f2c1ed85f9b89bcec4764255dbe
Hamid Reza Ansari <hamid.22.ansari@gmail.com>
2024-12-27 12:56:52 +0330

    Initial Commit

---
+++ WifiConfigure/WifiConfigureApp.swift
@@ -0,0 +1,17 @@
+//
+//  WifiConfigureApp.swift
+//  WifiConfigure
+//
+//  Created by Hamid on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WifiConfigureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
